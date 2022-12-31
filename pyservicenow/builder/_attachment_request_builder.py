d73c880bee28c1cd8ddfa06aad30a25217117c9e
michaeldcanady <michaeldcanady+github@gmail.com>
2022-12-30 23:18:35 -0500

    updating to match new pyrestsdk

--- pyservicenow/builder/_attachment_request_builder.py
+++ pyservicenow/builder/_attachment_request_builder.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, Iterable
+from typing import TYPE_CHECKING, Iterable, Optional, Union
 if TYPE_CHECKING:
     from pyservicenow.core import ServiceNowClient
 
 from pyrestsdk.requestbuilder import EntityRequestBuilder
 
 # internal imports
+from pyservicenow.types.models import ServiceNowQueryOption, ServiceNowHeaderOption, AttachmentEntry
 from pyservicenow.request import AttachmentEntryRequest, AttachmentEntryCollectionRequest
 
 class AttachmentRequestBuilder(EntityRequestBuilder):
@@ -25,8 +26,8 @@ class AttachmentRequestBuilder(EntityRequestBuilder):
 
         return self.Request(None)
 
-    def Request(self, options: Iterable) -> AttachmentEntryCollectionRequest:
-        return AttachmentEntryCollectionRequest(self.RequestUrl, self.Client, None)
+    def Request(self, options: Optional[Iterable[Union[ServiceNowQueryOption, ServiceNowHeaderOption]]]) -> AttachmentEntryCollectionRequest:
+        return AttachmentEntryCollectionRequest(AttachmentEntry, self.RequestUrl, self.Client, options)
     
     def id(self, sys_id: str) -> AttachmentEntryRequest:
         """Constructs a Table Entry Requst using the provided sys_id
@@ -38,4 +39,4 @@ class AttachmentRequestBuilder(EntityRequestBuilder):
             AttachmentEntryRequest: The constructed Table Entry Request
         """
 
-        return AttachmentEntryRequest(self.AppendSegmentToRequestUrl(sys_id), self.Client, None)
+        return AttachmentEntryRequest(AttachmentEntry, self.AppendSegmentToRequestUrl(sys_id), self.Client, None)
--- pyservicenow/builder/_now_request_builder.py
+++ pyservicenow/builder/_now_request_builder.py
@@ -7,6 +7,7 @@ from pyrestsdk.requestbuilder import EntityRequestBuilder
 # internal imports
 from pyservicenow.builder._table_request_builder import TableRequestBuilder
 from pyservicenow.builder._ui_request_builder import UIRequestBuilder
+from pyservicenow.builder._attachment_request_builder import AttachmentRequestBuilder
 
 class NowRequestBuilder(EntityRequestBuilder):
 
@@ -24,6 +25,11 @@ class NowRequestBuilder(EntityRequestBuilder):
         """
 
         return TableRequestBuilder(self.AppendSegmentToRequestUrl(f"/table/{table_name}"), self.Client)
+    
+    @property
+    def Attachment(self) -> AttachmentRequestBuilder:
+        
+        return AttachmentRequestBuilder(self.AppendSegmentToRequestUrl("attachment"), self.Client)
     
     @property
     def UI(self) -> UIRequestBuilder:
