bcecd68789029c1cfbf40ee2e735592d923c9c4e
David Canady <dmcanady@liberty.edu>
2023-01-03 10:14:02 -0500

    adding dependancies

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 from setuptools import setup
+from typing import List
 from pyservicenow import __version__, __module_name__
 import os
 
-directory = os.path.join(".","pyservicenow")
+def get_dependancies() -> List[str]:
+    dependancies = []
 
-_packages = [x[0].replace("\\",".") for x in os.walk(directory) if "__pycache__" not in x[0]]
+    with open("./requirements.txt","r") as file:
+        dependancies = file.read().split("\n")
+        dependancies = filter(None, dependancies)
+        dependancies = filter(bool, dependancies)
+        dependancies = filter(len, dependancies)
+        dependancies = list(filter(None, dependancies))
+
+    return dependancies
+
+def get_packages() -> List[str]:
+    directory = os.path.join(".","pyservicenow")
+
+    _packages = [x[0].replace("\\",".") for x in os.walk(directory) if "__pycache__" not in x[0]]
+
+    return _packages
 
 setup(
     name = __module_name__,
@@ -15,6 +31,7 @@ setup(
     license = "MIT",
     #keywords = "example documentation tutorial",
     url = "https://github.com/michaeldcanady/pyservicenow",
-    packages=_packages,
+    packages=get_packages(),
+    install_requires=get_dependancies()
     #long_description=read('README'),,
 )
