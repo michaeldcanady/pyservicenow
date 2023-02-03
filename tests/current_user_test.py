448a13e7cc4651acd444dcd051632898c51a6c25
David Canady <dmcanady@liberty.edu>
2023-02-03 12:31:05 -0500

    updating pyrestsdk

---
+++ tests/current_user_test.py
@@ -0,0 +1,16 @@
+import os
+
+from pyrestsdk.credential import BasicCredential
+
+from pyservicenow.core import ServiceNowClient
+from pyservicenow.types.models import AttachmentEntry
+
+def test_current_url():
+
+    creds = BasicCredential(os.environ["USERNAME"], os.environ["PASSWORD"])
+
+    client = ServiceNowClient(credential=creds, instance=os.environ["INSTANCE"])
+
+    attachment_request = client.Now.v2.ui_api.User.current_user
+    
+    assert attachment_request.request_url == client.base_url+"/ui/user/current_user"
